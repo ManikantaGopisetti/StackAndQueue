661cc998d75cb24fc8ed0c481a60b48bbfbfc464
ManikantaGopisetti <manikantagopisetti2@gmail.com>
2021-11-24 22:46:04 +0530

    Creating a queue using linked list to do queue operations

---
+++ queue/QueueDemo.java
@@ -0,0 +1,14 @@
+package com.bridgelabz.stackpractice.queue;
+
+public class QueueDemo {
+	public static void main(String[] args) {
+		
+		Queue queue= new Queue();
+		queue.enquee(56);
+		queue.enquee(30);
+		queue.enquee(70);
+		queue.displayQueue();
+		queue.size();
+		}
+
+}
